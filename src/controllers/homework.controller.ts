1708d8d68b0e1a42f3d3c423756b6193946ca95d
barnabaskocsis <barna1349@gmail.com>
2020-11-08 16:45:22 +0100

    rest api finished
    rest api finished

--- src/controllers/homework.controller.ts
+++ src/controllers/homework.controller.ts
@@ -14,19 +14,27 @@ homeworkRouter
     next();
   })
 
-  // list all homweworks of a teacher
-  .get("/:id", async (req, res) => {
-    const homeworks = await req.homeworkRepository!.findAll({ teacher_id: parseInt(req.params.id) });
+  // teachers use to list all homeworks they created
+  .get("/", async (req, res) => {
+    const authUser = req.orm.em.getReference(User, req.user!.id);
+    const teacherid = authUser.id;
+    const homeworks = await req.homeworkRepository!.findAll({ teacher_id: teacherid });
     res.send(homeworks);
   })
 
-  // add new lesson for a teacher
-  .post("/newhomework", passport.authenticate("jwt", { session: false }), async (req, res) => {
+  // users use to access a homework page by its uuid
+  .get("/:uuid", async (req, res) => {
+    const homework = await req.homeworkRepository!.find({ uuid: req.params.uuid });
+    res.send(homework);
+  })
+
+  // teachers use to create new homeworks for students
+  .post("/newhomework", async (req, res) => {
     if (req.user!.role === UserRole.Teacher) {
       const { uuid, name, title, description, path_to_solution }: AuthenticationDto = req.body;
       const homework = new Homework();
 
-      wrap(homework).assign({...req.body, uuid: v4()}, { em: req.orm.em });
+      wrap(homework).assign({ ...req.body, uuid: v4() }, { em: req.orm.em });
 
       homework.owner = req.orm.em.getReference(User, req.user!.id);
 
@@ -35,12 +43,49 @@ homeworkRouter
       res.send(homework);
     }
     return res.sendStatus(403);
+  })
+
+  //teachers use to update a homework
+  .patch("/update/:uuid", async (req, res) => {
+    const authUser = req.orm.em.getReference(User, req.user!.id);
+    const teacherid = authUser.id;
+    const uuid = req.params.uuid;
+    const { name, title, description }: AuthenticationDto = req.body;
+    const homework = await req.homeworkRepository!.findOne({ uuid: req.params.uuid, owner_id: teacherid });
+    if (!homework) {
+      return res.sendStatus(404);
+    }
+    if (homework) {
+      const updateCount = await req.homeworkRepository?.nativeUpdate({ uuid }, req.body);
+      if (updateCount) {
+        return res.sendStatus(200);
+      }
+    }
+    return res.sendStatus(401);
+  })
+
+  //teachers use to delete a homework
+  .delete("/delete/:uuid", async (req, res) => {
+    const authUser = req.orm.em.getReference(User, req.user!.id);
+    const teacherid = authUser.id;
+    const uuid = req.params.uuid;
+    const homework = await req.homeworkRepository!.findOne({ uuid: req.params.uuid, owner_id: teacherid });
+    if (!homework) {
+      return res.sendStatus(404);
+    }
+    if (homework) {
+      const deletedCount = await req.homeworkRepository?.nativeDelete({ uuid });
+      if (deletedCount) {
+        return res.sendStatus(200);
+      }
+    }
+    return res.sendStatus(401);
   });
 
 interface AuthenticationDto {
-  uuid: string;
-  name: number;
-  title: string;
+  uuid:string;
+  name: string;
+  title: number;
   description: string;
   path_to_solution: string;
 }
--- src/controllers/studentlesson.controller.ts
+++ src/controllers/studentlesson.controller.ts
@@ -2,6 +2,7 @@ import { Collection, wrap } from "@mikro-orm/core";
 import { Router } from "express";
 import { StudentLesson } from "../entities/studentlesson";
 import { User, UserRole } from "../entities/user";
+import { Lesson } from "../entities/lesson";
 import { hashPassword } from "../security/password-utils";
 import { passport } from "../security/passport";
 import { v4 } from "uuid";
@@ -14,16 +15,46 @@ studentlessonRouter
     next();
   })
 
-  // list all homweworks of a teacher
-  .get("/:id", async (req, res) => {
-    const homeworks = await req.studentLessonRepository!.findAll({ teacher_id: parseInt(req.params.id) });
-    res.send(homeworks);
+  // students use to check their booked lessons
+  .get("/", async (req, res) => {
+    const authUser = req.orm.em.getReference(User, req.user!.id);
+    const studentlessons = await req.studentLessonRepository!.findAll({ id: authUser.id });
+    res.send(studentlessons);
   })
 
+  // students use to book a lesson by {lesson_id}
+  .post("/book/:id",async (req, res) => {
+    const { date }: AuthenticationDto = req.body;
+    const studentlesson = new StudentLesson();
+
+    wrap(studentlesson).assign(req.body, { em: req.orm.em });
+
+    studentlesson.user = req.orm.em.getReference(User, req.user!.id);
+    studentlesson.lesson = req.orm.em.getReference(Lesson, parseInt(req.params.id));
+
+    await req.studentLessonRepository!.persistAndFlush(studentlesson);
+
+    res.send(studentlesson);
+  })
+
+  // students use to delete a booked lesson
+  .delete("/delete/:id", async (req, res) => {
+    const authUser = req.orm.em.getReference(User, req.user!.id);
+    const studentid = authUser.id;
+    const id = req.params.id;
+    const lesson = await req.studentLessonRepository!.findOne({ id: parseInt(req.params.id) ,user_id: studentid});
+    if (!lesson) {
+      return res.sendStatus(401);
+    }
+    if (lesson) {
+      const deletedCount = await req.studentLessonRepository?.nativeDelete({ id });
+      if (deletedCount) {
+        return res.sendStatus(200);
+      }
+    }
+    return res.sendStatus(404);
+  });
+
 interface AuthenticationDto {
-  uuid: string;
-  name: number;
-  title: string;
-  description: string;
-  path_to_solution: string;
+  date: Date;
 }
